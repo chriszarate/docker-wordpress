f4b16235fdbdd5503868ae35f77480295efdc4e9
chriszarate <chris@zarate.org>
2017-09-24 20:59:11 -0400

    Variablize some things and make uploads chown more robust.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,15 +2,18 @@
 
 set -ex
 
+ROOT_DIR=/var/www/html
+WEB_USER=www-data
+
 # Copy WordPress core.
 if ! [ -e wp-includes/version.php ]; then
-  tar cf - --one-file-system -C /usr/src/wordpress . | tar xf - --owner="$(id -u www-data)" --group="$(id -g www-data)"
+  tar cf - --one-file-system -C /usr/src/wordpress . | tar xf - --owner="$(id -u $WEB_USER)" --group="$(id -g $WEB_USER)"
   echo "WordPress has been successfully copied to $(pwd)"
 fi
 
 # Seed wp-content directory if requested.
 if [ -d /tmp/wordpress/init-wp-content ]; then
-  tar cf - --one-file-system -C /tmp/wordpress/init-wp-content . | tar xf - -C ./wp-content --owner="$(id -u www-data)" --group="$(id -g www-data)"
+  tar cf - --one-file-system -C /tmp/wordpress/init-wp-content . | tar xf - -C ./wp-content --owner="$(id -u $WEB_USER)" --group="$(id -g $WEB_USER)"
   echo "Seeded wp-content directory from /tmp/wordpress/init-wp-content."
 fi
 
@@ -31,8 +34,8 @@ fi
 sed -i -E "s#^url: .*#url: ${WORDPRESS_SITE_URL:-http://project.dev}#" /etc/wp-cli/config.yml
 
 # Create WordPress config.
-if ! [ -f /var/www/html/wp-config.php ]; then
-  runuser www-data -s /bin/sh -c "\
+if ! [ -f $ROOT_DIR/wp-config.php ]; then
+  runuser $WEB_USER -s /bin/sh -c "\
   wp config create \
     --dbhost=\"${WORDPRESS_DB_HOST:-mysql}\" \
     --dbname=\"${WORDPRESS_DB_NAME:-wordpress}\" \
@@ -45,8 +48,9 @@ PHP"
 fi
 
 # Make sure uploads directory exists and is writeable.
-chown www-data:www-data /var/www/html/wp-content
-runuser www-data -s /bin/sh -c "mkdir -p /var/www/html/wp-content/uploads"
+mkdir -p $ROOT_DIR/wp-content/uploads
+chown $WEB_USER:$WEB_USER $ROOT_DIR/wp-content
+chown -R $WEB_USER:$WEB_USER $ROOT_DIR/wp-content/uploads
 
 # MySQL may not be ready when container starts.
 set +ex
@@ -58,7 +62,7 @@ done
 set -ex
 
 # Install WordPress.
-runuser www-data -s /bin/sh -c "\
+runuser $WEB_USER -s /bin/sh -c "\
 wp core $([ "$WORDPRESS_INSTALL_TYPE" == "multisite" ] && echo "multisite-install" || echo "install") \
   --title=\"${WORDPRESS_SITE_TITLE:-Project}\" \
   --admin_user=\"${WORDPRESS_SITE_USER:-wordpress}\" \
@@ -68,7 +72,7 @@ wp core $([ "$WORDPRESS_INSTALL_TYPE" == "multisite" ] && echo "multisite-instal
   --skip-email"
 
 # Update rewrite structure.
-runuser www-data -s /bin/sh -c "\
+runuser $WEB_USER -s /bin/sh -c "\
 wp option update permalink_structure \"${WORDPRESS_PERMALINK_STRUCTURE:-/%year%/%monthnum%/%postname%/}\" \
   --skip-themes \
   --skip-plugins"
@@ -76,22 +80,22 @@ wp option update permalink_structure \"${WORDPRESS_PERMALINK_STRUCTURE:-/%year%/
 # Activate plugins. Install if it cannot be found locally.
 if [ -n "$WORDPRESS_ACTIVATE_PLUGINS" ]; then
   for plugin in $WORDPRESS_ACTIVATE_PLUGINS; do
-    if ! [ -d "/var/www/html/wp-content/plugins/$plugin" ]; then
-      runuser www-data -s /bin/sh -c "wp plugin install \"$plugin\""
+    if ! [ -d "$ROOT_DIR/wp-content/plugins/$plugin" ]; then
+      runuser $WEB_USER -s /bin/sh -c "wp plugin install \"$plugin\""
     fi
   done
 
   # shellcheck disable=SC2086
-  runuser www-data -s /bin/sh -c "wp plugin activate $WORDPRESS_ACTIVATE_PLUGINS"
+  runuser $WEB_USER -s /bin/sh -c "wp plugin activate $WORDPRESS_ACTIVATE_PLUGINS"
 fi
 
 # Activate theme. Install if it cannot be found locally.
 if [ -n "$WORDPRESS_ACTIVATE_THEME" ]; then
-  if ! [ -d "/var/www/html/wp-content/themes/$WORDPRESS_ACTIVATE_THEME" ]; then
-    runuser www-data -s /bin/sh -c "wp theme install \"$WORDPRESS_ACTIVATE_THEME\""
+  if ! [ -d "$ROOT_DIR/wp-content/themes/$WORDPRESS_ACTIVATE_THEME" ]; then
+    runuser $WEB_USER -s /bin/sh -c "wp theme install \"$WORDPRESS_ACTIVATE_THEME\""
   fi
 
-  runuser www-data -s /bin/sh -c "wp theme activate \"$WORDPRESS_ACTIVATE_THEME\""
+  runuser $WEB_USER -s /bin/sh -c "wp theme activate \"$WORDPRESS_ACTIVATE_THEME\""
 fi
 
 exec "$@"
